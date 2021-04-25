1c22706b3aa599495606994cf9955e294f37f458
ruamrdd <Dmitry.MURINOV@raiffeisen.ru>
2021-04-25 12:34:04 +0300

    First Git spring project. Initial commit.
    Needs wildfy 23 and configuration:
    Main tab:
    JBoss 23.0.0.Final
    URL: http://localhost:8080/spring-1.0-SNAPSHOT/
    
    JDK 1.8
    
    Before launch:
    Build
    Build spring:war exploded
    
    Deployments tab:
    sping:war exploded
    
    Logs tab:
    server.log /**path to log wildfly*/

---
+++ src/main/java/ru/learn/spring/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package ru.learn.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"ru.learn.spring"})
+public class ApplicationConfig {
+
+
+
+}
